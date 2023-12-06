8cfd640bd22106041f8a8b2e2a2a4a7e3f06a4c0
Ahmed <ahmed.m.almousawi@gmail.com>
2023-12-06 00:09:24 -0700

    old delta p calculations

---
+++ analysis2.py
@@ -0,0 +1,33 @@
+import numpy as np
+import CoolProp.CoolProp as CP
+import pandas as pd
+
+fluid_name = "Air"
+temperature =  449.15 # Temperature in K
+pressure = 1300000  # Pressure in Pa
+
+#Obtain dynamic viscosity (mu) for air
+viscosity = CP.PropsSI("V", "T", temperature, "P", pressure, fluid_name)
+density = CP.PropsSI("D", "T", temperature, "P", pressure, fluid_name)
+
+
+m_dot = 3
+
+diameters = np.linspace(0.01, 0.5, 100)
+length = np.linspace(0.1, 10, 100)
+
+solution = np.zeros((len(diameters), len(length)))
+
+i=0
+
+for d in diameters:
+    j=0
+    for l in length:
+        delta_P = (128 * m_dot * viscosity * l) / (np.pi * density * d)
+        solution[i, j] = delta_P
+        j += 1
+    i += 1  
+
+df = pd.DataFrame(solution)
+df.to_excel('output.xlsx', index=False)
+print(solution)
